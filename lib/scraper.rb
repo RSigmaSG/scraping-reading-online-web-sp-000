ed69e299434163ba818446c53cdd30ae5e92ef6f
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-27 03:13:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC
+puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
